b95901193b1be28238679ca28b8b6c9c3244f8b3
旷晓鹏 <kuangxiaopeng@139.com>
2019-02-26 16:26:13 +0800

    update

--- plugins/td-sso/handler.lua
+++ plugins/td-sso/handler.lua
@@ -7,47 +7,48 @@
 --
 
 local BasePlugin = require "kong.plugins.base_plugin"
+local ngx_log = ngx.log
+local INFO = ngx.INFO
 
 local TdSsoHandler = BasePlugin:extend()
-TdSsoHandler.PRIORITY = 1001
 
 function TdSsoHandler:new()
     TdSsoHandler.super.new(self, "td-sso")
 end
 
-function TdSsoHandler:access(conf)
+function TdSsoHandler:header_filter(conf)
     TdSsoHandler.super.access(self)
 
     local request_uri = ngx.var.request_uri or ""
-    print("================")
-    print("request_uri")
-    print(request_uri)
-    print("================")
+    ngx_log(INFO, "================")
+    ngx_log(INFO, "request_uri")
+    ngx_log(INFO, request_uri)
+    ngx_log(INFO, "================")
     -- 获取cookies
     local cookie = require "resty.cookie"
     local ck = cookie:new()
     local oauthToken, err = ck:get(conf.cookie_name)
-    print("================")
-    print("request_uri")
-    print(request_uri)
-    print("================")
---
---    -- 如果cookie 不存在
---    if not oauthToken then
---        ngx.log("================")
---        ngx.log("跳转")
---        ngx.log(conf.oauth_url)
---        ngx.log("================")
---        ngx.log(ngx.ERR, err)
---        ngx.redirect(conf.oauth_url, ngx.HTTP_MOVED_PERMANENTLY)
---    else
---        ngx.log("================")
---        ngx.log("跳转")
---        ngx.log(oauthToken)
---        ngx.log("================")
---        --TODO 校验cookie
---        ngx.req.set_header("Authorization", "Bearer " .. oauthToken)
---    end
+    ngx_log(INFO, "================")
+    ngx_log(INFO, "request_uri")
+    ngx_log(INFO, request_uri)
+    ngx_log(INFO, "================")
+    --
+    --    -- 如果cookie 不存在
+    --    if not oauthToken then
+    --        ngx.log("================")
+    --        ngx.log("跳转")
+    --        ngx.log(conf.oauth_url)
+    --        ngx.log("================")
+    --        ngx.log(ngx.ERR, err)
+    --        ngx.redirect(conf.oauth_url, ngx.HTTP_MOVED_PERMANENTLY)
+    --    else
+    --        ngx.log("================")
+    --        ngx.log("跳转")
+    --        ngx.log(oauthToken)
+    --        ngx.log("================")
+    --        --TODO 校验cookie
+    --        ngx.req.set_header("Authorization", "Bearer " .. oauthToken)
+    --    end
 end
 
 return TdSsoHandler
