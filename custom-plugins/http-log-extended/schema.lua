6acfa2e3b545527210a44a3e83090a91b44af59b
旷晓鹏 <kuangxiaopeng@139.com>
2019-02-26 10:26:42 +0800

    update dir

---
+++ custom-plugins/http-log-extended/schema.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: kuangxiaopeng
+-- Date: 2019/2/20
+-- Time: 00:32
+-- To change this template use File | Settings | File Templates.
+--
+
+return {
+  fields = {
+    http_endpoint = { required = true, type = "url" },
+    timeout = { default = 10000, type = "number" },
+    keepalive = { default = 60000, type = "number" },
+    log_request_body = { type = "boolean", default = false },
+    log_response_body = { type = "boolean", default = false }
+  }
+}
