6f9536fd300964f840bcc6e841eeebd1f48581a1
旷晓鹏 <kuangxiaopeng@139.com>
2019-02-26 23:49:05 +0800

    update

--- plugins/td-sso/handler.lua
+++ plugins/td-sso/handler.lua
@@ -22,6 +22,11 @@ function TdSsoHandler:access(conf)
     local ck = cookie:new()
     local accessToken, accessTokenErr = ck:get(conf.cookie_token_key)
 
+    ngx.log(ngx.ERR, "token cookie_token_key:" .. conf.cookie_token_key .. " \n")
+
+    ngx.log(ngx.ERR, "token cookie_client_key:" .. conf.cookie_client_key .. " \n")
+
+
     if not accessTokenErr then
         ngx.log(ngx.ERR, "token error \n")
         ngx.log(ngx.ERR, accessTokenErr)
@@ -50,29 +55,29 @@ function TdSsoHandler:access(conf)
     --TODO 校验cookie
     ngx.log(ngx.ERR, "================\n")
     ngx.log(ngx.ERR, "设置header\n")
-    ngx.log(ngx.ERR, "access herer\n"..clientId)
---    local httpc = http:new()
---    local res, err = httpc:request_uri(conf.oauth_check_url .. "?appVersion=1.0&requestClientId=" .. clientId .. "&access_token=" .. accessToken, {
---        method = "GET",
---        ssl_verify = false,
---        headers = {
---            ["Content-Type"] = "application/x-www-form-urlencoded",
---        }
---    })
---
---    if not err then
---        ngx.log(ngx.ERR, "err =====\n")
---        ngx.log(ngx.ERR, err)
---        ngx.log(ngx.ERR, "res===\n")
---        ngx.log(ngx.ERR, res)
---        ngx.say(err)
---        ngx.exit(ngx.HTTP_UNAUTHORIZED)
---    end
---
---    ngx.log(ngx.ERR, "================\n")
---    ngx.log(ngx.ERR, "认证结果\n")
---    ngx.log(ngx.ERR, res)
---    ngx.log(ngx.ERR, "\n================\n")
+    ngx.log(ngx.ERR, "access herer\n" .. clientId)
+    --    local httpc = http:new()
+    --    local res, err = httpc:request_uri(conf.oauth_check_url .. "?appVersion=1.0&requestClientId=" .. clientId .. "&access_token=" .. accessToken, {
+    --        method = "GET",
+    --        ssl_verify = false,
+    --        headers = {
+    --            ["Content-Type"] = "application/x-www-form-urlencoded",
+    --        }
+    --    })
+    --
+    --    if not err then
+    --        ngx.log(ngx.ERR, "err =====\n")
+    --        ngx.log(ngx.ERR, err)
+    --        ngx.log(ngx.ERR, "res===\n")
+    --        ngx.log(ngx.ERR, res)
+    --        ngx.say(err)
+    --        ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    --    end
+    --
+    --    ngx.log(ngx.ERR, "================\n")
+    --    ngx.log(ngx.ERR, "认证结果\n")
+    --    ngx.log(ngx.ERR, res)
+    --    ngx.log(ngx.ERR, "\n================\n")
 end
 
 return TdSsoHandler
